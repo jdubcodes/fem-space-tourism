bd97fe07ca341cad42599fd7f8443a2c16e46cda
Joseph Wilde <jwilde19@gmail.com>
2023-10-24 22:09:34 -0400

    Create utility classes and setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      paddingTop: '3rem',
+      paddingX: '10.3rem',
+      paddingBottom: '8.2rem',
+    },
+    colors: {
+      dark: '#0B0D17',
+      light: '#D0D6F9',
+      white: '#FFFFFF',
+    },
+    fontSize: {
+      h1: '9.375rem',
+      h2: '6.25rem',
+      h3: '3.5rem',
+      h4: '2rem',
+      sub1: '1.75rem',
+      sub2: '0.875rem',
+      nav: '1rem',
+      base: '1.125rem',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
