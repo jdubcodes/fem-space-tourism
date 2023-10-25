d15d73075c23f13ef9af3495c70a3de88db68286
Joseph Wilde <jwilde19@gmail.com>
2023-10-24 22:08:28 -0400

    Finish main page elements

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from './components/Navbar'
 
 export const metadata = {
   title: 'Space Tourism',
@@ -7,8 +8,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className='w-screen h-screen'>
+      <body className='w-full h-full bg-[url("/home/background-home-desktop.jpg")] bg-cover'>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,27 @@
-import Header from './components/Header'
+import { barlowCondensed, barlow, bellefair } from './fonts'
 
 export default function Home() {
   return (
-    <main
-      className={`h-screen w-screen bg-[url("/home/background-home-desktop.jpg")] bg-cover`}
-    >
-      <Header />
+    <main className='pt-12 px-[10.3rem] pb-[8.2rem] flex justify-items-center'>
+      <div className='grid grid-cols-2 h-full w-full place-items-end m-w-6xl'>
+        <div className='px-10'>
+          <p className={`${barlowCondensed.className} h5`}>
+            So, you want to travel to
+          </p>
+          <span className={`${bellefair.className} h1`}>Space</span>
+          <p className={`${barlow.className} body-text`}>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div
+          className={`${bellefair.className} w-[17.125rem] h-[17.125rem] flex justify-center items-center bg-white rounded-full uppercase text-h4 tracking-[0.125rem] text-[#0B0D17] cursor-pointer`}
+        >
+          Explore
+        </div>
+      </div>
     </main>
   )
 }
