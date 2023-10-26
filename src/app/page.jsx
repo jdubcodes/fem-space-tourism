a505575ac51e3769d0ceb64d6f1d5dc239f1f60c
Joseph Wilde <jwilde19@gmail.com>
2023-10-26 18:53:48 -0400

    Make main page more responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,9 @@ import { barlowCondensed, barlow, bellefair } from './fonts'
 
 export default function Home() {
   return (
-    <main className='page-container h-screen absolute top-0  bg-[url("/home/background-home-desktop.jpg")] bg-cover overflow-hidden'>
-      <div className='sticky bottom-0 grid grid-cols-2 h-full w-full place-items-end m-w-6xl'>
-        <div className='px-10'>
+    <main className='page-container h-screen w-screen absolute top-0 bg-[url("/home/background-home-desktop.jpg")] bg-cover overflow-hidden'>
+      <div className='bottom-0 mx-auto grid grid-cols-2 h-full w-full place-items-end max-w-[69.125rem]'>
+        <div>
           <p className={`${barlowCondensed.className} h5`}>
             So, you want to travel to
           </p>
@@ -19,7 +19,7 @@ export default function Home() {
         </div>
         <Link href='/destination'>
           <div
-            className={`${bellefair.className} w-[17.125rem] h-[17.125rem] flex justify-center items-center bg-white rounded-full uppercase text-h4 tracking-[0.125rem] text-[#0B0D17] cursor-pointer`}
+            className={`${bellefair.className} w-[17.125rem] h-[17.125rem] flex justify-center items-center bg-white rounded-full uppercase text-h4 tracking-[0.125rem] text-[#0B0D17] cursor-pointer z-20`}
           >
             Explore
             <span className='absolute w-[17.125rem] h-[17.125rem] opacity-0 bg-white rounded-full hover:opacity-[0.1036] hover:w-[28.125rem] hover:h-[28.125rem] origin-center duration-700 ease-in-out'></span>
