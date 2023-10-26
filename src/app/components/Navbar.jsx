c18eaf09f321e78e61d3cc0d2fb7efadad0e7c4c
Joseph Wilde <jwilde19@gmail.com>
2023-10-26 18:52:03 -0400

    Update navbar responsiveness

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,35 +4,35 @@ import { barlowCondensed } from '../fonts'
 
 export default function Navbar() {
   return (
-    <nav className={`${barlowCondensed.className} pt-10 pl-[55px]`}>
-      <div className='flex items-center justify-between'>
-        <Link href='/' className='z-10 mr-auto'>
+    <nav className={`${barlowCondensed.className} pt-16 px-[10.45rem]`}>
+      <div className='flex items-center justify-end max-w-[69.125rem] mx-auto'>
+        <Link href='/' className='z-10 absolute left-11'>
           <Image src='/shared/logo.svg' alt='Logo' width={48} height={48} />
         </Link>
-        <span className='translate-x-8 bg-white opacity-[0.2515] w-[450px] h-[0.0625rem] z-20 flex-shrink-0'></span>
-        <div className='w-[51.875rem] pl-[7.72rem] pr-[10.45rem] bg-[rgba(255, 255, 255, 0.04)] backdrop-blur-[5.5rem] py-9 z-10'>
-          <ul className='relative -left-7 flex justify-between gap-12 text-lg'>
-            <li>
-              <Link href='/'>
-                <span className='font-bold mr-2'>00</span>HOME
-              </Link>
-            </li>
-            <li>
-              <Link href='/destination'>
-                <span className='font-bold mr-2'>01</span>DESTINATION
-              </Link>
-            </li>
-            <li>
-              <Link href='/crew'>
-                <span className='font-bold mr-2'>02</span>CREW
-              </Link>
-            </li>
-            <li>
-              <Link href='/technology'>
-                <span className='font-bold mr-2'>03</span>TECHNOLOGY
-              </Link>
-            </li>
-          </ul>
+        <ul className='flex gap-14 text-lg z-20'>
+          <li>
+            <Link href='/'>
+              <span className='font-bold mr-2'>00</span>HOME
+            </Link>
+          </li>
+          <li>
+            <Link href='/destination'>
+              <span className='font-bold mr-2'>01</span>DESTINATION
+            </Link>
+          </li>
+          <li>
+            <Link href='/crew'>
+              <span className='font-bold mr-2'>02</span>CREW
+            </Link>
+          </li>
+          <li>
+            <Link href='/technology'>
+              <span className='font-bold mr-2'>03</span>TECHNOLOGY
+            </Link>
+          </li>
+        </ul>
+        <div className='absolute right-0 nav-bg z-10'>
+          <span className='w-[30vw] h-[0.0625rem] fixed left-0 -translate-x-[28vw] bg-white opacity-[0.2515] z-20'></span>
         </div>
       </div>
     </nav>
