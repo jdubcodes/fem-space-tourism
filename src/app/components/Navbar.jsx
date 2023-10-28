bbd23bcb27f69aa594578ae47cf65faf62d62077
Joseph Wilde <jwilde19@gmail.com>
2023-10-27 21:30:37 -0400

    Finish tablet and large screen navbar responsiveness

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -10,8 +10,8 @@ export default function Navbar() {
       <Link href='/' className='z-10 absolute left-[6vw] -translate-y-[12px]'>
         <Image src='/shared/logo.svg' alt='Logo' width={48} height={48} />
       </Link>
-      <div className='flex items-center justify-between max-w-[69.125rem] mx-auto'>
-        <span className='left-0 w-[478px] h-[0.0625rem] bg-white opacity-[0.2515] z-20'></span>
+      <div className='flex items-center justify-end xl:justify-between max-w-[69.125rem] mx-auto'>
+        <span className='left-0 w-[478px] h-[0.0625rem] bg-white opacity-[0.2515] z-20 hidden xl:inline'></span>
         <ul className='flex gap-14 text-lg z-20'>
           {menuItems.map((item) => (
             <li>
@@ -19,13 +19,15 @@ export default function Navbar() {
                 href={item.path}
                 className='pb-9 border-b-[3px] border-hidden border-[#979797] hover:border-solid ease-out uppercase'
               >
-                <span className='font-bold mr-2'>{item.number}</span>
+                <span className='font-bold mr-2 hidden xl:inline'>
+                  {item.number}
+                </span>
                 {item.text}
               </Link>
             </li>
           ))}
         </ul>
-        <div className='absolute right-0 nav-bg z-10'></div>
+        <div className='absolute right-0 w-[75vw] lg:w-[65vw] xl:w-[57vw] nav-bg z-10'></div>
       </div>
     </nav>
   )
