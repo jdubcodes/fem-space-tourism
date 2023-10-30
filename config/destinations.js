a47bc77c25696205de4d0ae831be493dbc5997fd
Joseph Wilde <jwilde19@gmail.com>
2023-10-30 19:53:07 -0400

    Add paths to destination file

--- config/destinations.js
+++ config/destinations.js
@@ -4,24 +4,28 @@ const destinations = [
     description: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.`,
     distance: '384,400 km',
     time: '3 days',
+    path: '/destination/image-moon.webp',
   },
   {
     destination: 'Mars',
     description: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!`,
     distance: '225 mil. km',
     time: '9 months',
+    path: '/destination/image-mars.webp',
   },
   {
     destination: 'Europa',
     description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.`,
     distance: '628 mil. km',
     time: '3 years',
+    path: '/destination/image-europa.webp',
   },
   {
     destination: 'Titan',
     description: `The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.`,
     distance: '1.6 bil. km',
     time: '7 years',
+    path: '/destination/image-titan.webp',
   },
 ]
 
