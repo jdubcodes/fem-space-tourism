a4817da45b89385112e9eea160e844473bc7d6e8
Joseph Wilde <jwilde19@gmail.com>
2023-11-02 14:37:08 -0400

    Add styling for larger screens

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -26,13 +26,13 @@ export default function page() {
 
   return (
     <main className='pt-[25vh] px-[10.45rem] h-screen w-screen absolute top-0 bg-[url("/crew/background-crew-desktop.jpg")] bg-cover bg-center overflow-hidden'>
-      <div className='bottom-0 h-full w-full max-w-[69.125rem] mx-auto grid grid-cols-1 lg:grid-cols-2 place-items-start gap-10 lg:gap-0 text-center lg:text-left'>
+      <div className='bottom-0 h-full max-h-[700px] w-full max-w-[69.125rem] mx-auto grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-0 text-center lg:text-left'>
         {/* Left side */}
         <div className='flex flex-col w-full h-full pr-10'>
           <h3 className={`${barlowCondensed.className} h5`}>
             <span className='opacity-25 mr-4'>02</span>Meet Your Crew
           </h3>
-          <div className='flex flex-col mb-auto pt-36'>
+          <div className='flex flex-col mb-auto pt-36 h-[486px]'>
             <h4 className={`${bellefair.className} h4 opacity-50`}>
               {crew[crewIndex()].title}
             </h4>
@@ -43,7 +43,7 @@ export default function page() {
               {crew[crewIndex()].desc}
             </p>
             {/* Add menu buttons */}
-            <div className='flex gap-6 fixed bottom-28'>
+            <div className='flex gap-6 mt-auto relative -bottom-6'>
               {crew.map((crew, index) => (
                 <button
                   key={index}
@@ -62,9 +62,9 @@ export default function page() {
         <Image
           src={crew[crewIndex()].path}
           alt={crew[crewIndex()].name}
-          width={568}
-          height={735}
-          className='pl-10 self-end'
+          width={800}
+          height={800}
+          className='pl-10 self-end relative bottom-0 right-0 overflow-hidden'
         />
       </div>
     </main>
