8505c814243f9267722081ae840e9710162f4b22
Joseph Wilde <jwilde19@gmail.com>
2023-11-10 14:35:06 -0500

    Finish responsiveness

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { barlowCondensed } from '../fonts'
 
 import { gsap } from 'gsap'
 
+import MobileButton from './MobileButton'
 import menuItems from '../../../config/menuItems'
 
 export default function Navbar() {
@@ -21,8 +22,8 @@ export default function Navbar() {
     const ctx = gsap.context((self) => {
       const nav = self.selector(navRef.current)
       tl.current = gsap.timeline().to(nav, { x: 0 }).reverse()
-    }, navRef) // <- Scope!
-    return () => ctx.revert() // <- Cleanup!
+    }, navRef)
+    return () => ctx.revert()
   }, [])
 
   const handleClick = () => {
@@ -31,94 +32,68 @@ export default function Navbar() {
   }
 
   return (
-    <header>
+    <header className={`${barlowCondensed.className}`}>
       <Link
         href='/'
-        className='z-10 absolute w-10 h-10 left-6 top-6 md:w-12 md:h-12 md:left-[6vw] md:translate-y-[28px]'
+        className='w-10 h-10 absolute left-8 top-8 z-10 md:w-12 md:h-12 md:left-[6vw] md:translate-y-7'
       >
         <Image src='/shared/logo.svg' alt='Logo' width={48} height={48} />
       </Link>
       {/* mobile menu */}
-      <div>
-        {/* hamburger button */}
-        <div className='md:hidden'>
-          <button
-            onClick={handleClick}
-            className='flex flex-col justify-center items-center absolute right-10 top-10 z-50'
-          >
-            <span
-              className={`bg-light block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm ${
-                      isOpen ? 'rotate-45 translate-y-1.5' : '-translate-y-0.5'
-                    }`}
-            ></span>
-            <span
-              className={`bg-light block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm my-1 ${
-                      isOpen ? 'opacity-0' : 'opacity-100'
-                    }`}
-            ></span>
-            <span
-              className={`bg-light block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm ${
-                      isOpen ? '-rotate-45 -translate-y-1.5' : 'translate-y-0.5'
-                    }`}
-            ></span>
-          </button>
-          {/* background */}
-          <div
-            className='h-screen p-10 nav-bg translate-x-60 fixed right-0'
-            ref={navRef}
-          >
-            {/* desktop menu */}
-            <nav className='pt-28'>
-              <ul
-                className={`${barlowCondensed.className} flex flex-col gap-8 nav-text z-20`}
-              >
-                {menuItems.map((item, index) => (
-                  <li key={index}>
-                    <Link
-                      href={item.path}
-                      onClick={handleClick}
-                      className='hover:text-light'
-                    >
-                      <span className='font-bold mr-2'>{item.number}</span>
-                      {item.text}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+      <div className='md:hidden'>
+        {/* hamburger menu button */}
+        <MobileButton handleClick={handleClick} isOpen={isOpen} />
+        <div
+          className='h-screen p-10 nav-bg translate-x-60 fixed right-0'
+          ref={navRef}
+        >
+          <nav className='pt-28'>
+            <ul className='flex flex-col gap-8 nav-text z-20'>
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    href={item.path}
+                    onClick={handleClick}
+                    className='hover:text-light'
+                  >
+                    <span className='font-bold mr-2'>{item.number}</span>
+                    {item.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
       </div>
-      <nav
-        className={`${barlowCondensed.className} pt-16 px-[3rem] md:px-[6rem] lg:px-[10.45rem] absolute`}
-      >
-        <div className='flex items-center justify-end xl:justify-between max-w-container mx-auto'>
-          <span className='left-0 w-[478px] h-[0.0625rem] bg-white opacity-[0.2515] z-20 hidden xl:hidden'></span>
-          <ul className='hidden md:flex gap-14 text-lg z-20'>
-            {menuItems.map((item, index) => (
-              <li key={index}>
-                <Link
-                  href={item.path}
-                  className={
-                    pathname == item.path
-                      ? 'pb-9 border-b-[3px] border-white uppercase'
-                      : 'pb-9 border-b-[3px] border-hidden border-[#979797] hover:border-solid ease-out uppercase'
-                  }
-                >
-                  <span className='font-bold mr-2 hidden xl:inline'>
-                    {item.number}
-                  </span>
-                  {item.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className='hidden absolute right-0 w-[65vw] lg:w-[60vw] xl:w-[57vw] nav-bg z-10 md:inline-block'></div>
+      {/* desktop menu */}
+      <div className='pt-16 px-24 w-full hidden fixed md:inline lg:px-[10.45rem] z-20'>
+        <div className='flex items-center justify-end max-w-container mx-auto lg:justify-between'>
+          <span className='w-full h-nav-line max-w-line-tablet xl:max-w-line-desktop justify-self-start opacity-line bg-white hidden lg:inline'></span>
+          <nav>
+            <ul className='flex gap-7 text-nav lg:gap-10'>
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    href={item.path}
+                    className={
+                      pathname == item.path
+                        ? 'pb-[2.125rem] border-b-[3px] border-white uppercase'
+                        : 'pb-[2.125rem] border-b-[3px] border-hidden border-[#979797] ease-out uppercase hover:border-solid'
+                    }
+                  >
+                    <span className='font-bold mr-2 hidden xl:inline'>
+                      {item.number}
+                    </span>
+                    {item.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-      </nav>
+      </div>
+      {/* Nav desktop background */}
+      <div className='hidden absolute right-0 top-7 w-[63vw] md:inline lg:w-[60vw] xl:w-[57vw] nav-bg'></div>
     </header>
   )
 }
