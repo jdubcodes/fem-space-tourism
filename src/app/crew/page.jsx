32dc2e0c6e06d7c343c1f5ede1d04c34686a3648
Joseph Wilde <jwilde19@gmail.com>
2023-11-15 21:30:54 -0500

    Finish tablet layout

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -15,24 +15,24 @@ export default function page() {
 
   return (
     <main
-      className={`${barlowCondensed.className} min-h-screen p-crew-mobile grid items-center justify-items-center bg-crew-mobile bg-cover bg-center sm:bg-crew-tablet md:p-crew-tablet lg:bg-crew-desktop xl:p-crew-desktop`}
+      className={`${barlowCondensed.className} min-h-screen p-crew-mobile grid items-center justify-items-center bg-crew-mobile bg-cover bg-center sm:bg-crew-tablet md:p-crew-tablet md:h-screen md:overflow-hidden lg:bg-crew-desktop xl:p-crew-desktop`}
     >
-      <h3 className='text-desc-mobile self-start md:text-desc-tablet lg:text-desc-desktop'>
+      <h3 className='text-desc-mobile self-start md:justify-self-start md:text-desc-tablet lg:text-desc-desktop'>
         <span className='opacity-25 mr-4'>02</span>Meet Your Crew
       </h3>
       {/* main grid // 2 rows mobile + tablet // 2 columns desktop */}
-      <div className='max-w-container mx-auto flex flex-col gap-20 text-center lg:grid-rows-1 lg:grid-cols-2 lg:text-left'>
+      <div className='max-w-container mx-auto flex flex-col gap-20 text-center md:self-end lg:grid-rows-1 lg:grid-cols-2 lg:text-left'>
         {/* 2nd row or column flex */}
         <div className='flex flex-col justify-self-end'>
           {/* Crew image */}
-          <div className='w-full h-crew-mobile self-center justify-self-end border-b-[1px] border-[#383B4B]'>
-            <div className='w-crew-mobile h-crew-mobile mx-auto flex items-end relative overflow-hidden'>
+          <div className='w-full md:h-crew-tablet self-center justify-self-end border-b-[1px] border-[#383B4B] md:border-0 md:order-last'>
+            <div className='w-crew-mobile md:w-crew-tablet h-crew-mobile md:h-crew-tablet mx-auto flex items-end relative overflow-hidden'>
               <Image
                 src={crew[crewIndex(member)].path}
                 alt={crew[crewIndex(member)].name}
                 width={800}
                 height={800}
-                // layout='responsive'
+                layout='responsive'
                 className={
                   member === 'Douglas Hurley'
                     ? 'absolute top-0'
@@ -55,12 +55,12 @@ export default function page() {
               {crew[crewIndex(member)].name}
             </h4>
             <p
-              className={`${barlow.className} h-40 text-para-mobile md:text-para-tablet lg:text-para-desktop text-light `}
+              className={`${barlow.className} h-40 text-para-mobile md:h-20 md:text-para-tablet lg:text-para-desktop text-light `}
             >
               {crew[crewIndex(member)].desc}
             </p>
             {/* Add menu buttons */}
-            <div className='flex gap-4 -order-1 py-8 lg:gap-6 lg:mt-auto lg:-bottom-6'>
+            <div className='flex gap-4 -order-1 py-8 md:order-last lg:gap-6 lg:mt-auto lg:-bottom-6'>
               {crew.map((crew, index) => (
                 <button
                   key={index}
