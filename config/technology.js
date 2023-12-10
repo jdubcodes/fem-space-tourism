c8198a44be601e9b68c1ee58fa073d5b0e41338c
Joseph Wilde <jwilde19@gmail.com>
2023-12-09 21:25:20 -0500

    Change image path names

--- config/technology.js
+++ config/technology.js
@@ -3,22 +3,22 @@ const technology = [
     num: '1',
     tech: 'Launch Vehicle',
     desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-    pathLandscape: '/technology/image-launch-vehicle-landscape.jpg',
-    pathPortrait: '/technology/image-launch-vehicle-portrait.jpg',
+    pathMobile: '/technology/image-launch-vehicle-landscape.jpg',
+    pathDesktop: '/technology/image-launch-vehicle-portrait.jpg',
   },
   {
     num: '2',
     tech: 'Spaceport',
     desc: 'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
-    pathLandscape: '/technology/image-spaceport-landscape.jpg',
-    pathPortrait: '/technology/image-spaceport-portrait.jpg',
+    pathMobile: '/technology/image-spaceport-landscape.jpg',
+    pathDesktop: '/technology/image-spaceport-portrait.jpg',
   },
   {
     num: '3',
     tech: 'Space Capsule',
     desc: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-    pathLandscape: '/technology/image-space-capsule-landscape.jpg',
-    pathPortrait: '/technology/image-space-capsule-portrait.jpg',
+    pathMobile: '/technology/image-space-capsule-landscape.jpg',
+    pathDesktop: '/technology/image-space-capsule-portrait.jpg',
   },
 ]
 
