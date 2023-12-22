bdb382762ed0b4f24759955f04de3f58dca88a21
Joseph Wilde <jwilde19@gmail.com>
2023-12-22 13:02:57 -0500

    Import image handling file

--- src/app/technology/page.jsx
+++ src/app/technology/page.jsx
@@ -5,19 +5,21 @@ import Image from 'next/image'
 import technology from '../../../config/technology'
 import { techIndex } from '../../../config/findIndex'
 import { barlowCondensed, barlow, bellefair } from '../../../config/fonts'
+import { techImg } from '../../../config/techImages'
 
 export default function page() {
-  const [number, setNumber] = useState('1')
+  const [i, setI] = useState('1')
 
   useEffect(() => {
     techIndex()
-  }, [number])
+    console.log(window.innerWidth)
+  }, [i])
 
   return (
     <main
       className={`${barlowCondensed.className} h-screen w-screen p-tech-mobile absolute top-0 bg-tech-desktop bg-cover overflow-hidden md:p-tech-tablet`}
     >
-      <h1 className='max-w-container mb-12 uppercase text-center text-sub-mobile sm:pb-0 md:text-left md:text-sub-tablet xl:mx-auto xl:mb-[1.625rem] xl:text-sub-desktop'>
+      <h1 className='max-w-container mb-12 uppercase text-center text-sub-mobile sm:pb-0 md:text-sub-tablet xl:mx-auto xl:mb-[1.625rem] xl:text-left xl:text-sub-desktop'>
         <span className='opacity-25 mr-4'>03</span>Space launch 101
       </h1>
       <section
@@ -29,8 +31,8 @@ export default function page() {
           className='w-[100vw] h-[230px] relative overflow-hidden flex items-center justify-center md:h-[310px] xl:order-3'
         >
           <Image
-            src={technology[techIndex(number)].pathMobile}
-            alt={technology[techIndex(number)].tech}
+            src={techImg(i)}
+            alt={technology[techIndex(i)].tech}
             width={1024}
             height={527}
             responsive={true}
@@ -39,14 +41,14 @@ export default function page() {
         </div>
         <div
           id='buttons'
-          className='mt-[2.125rem] mb-[2rem] flex gap-4 lg:mr-20 lg:order-1 lg:flex-col'
+          className='mt-[2.125rem] mb-[2rem] flex gap-4 xl:mr-20 xl:order-1 xl:flex-col'
         >
           {technology.map((item) => (
             <button
               key={item.num}
-              onClick={() => setNumber(item.num)}
+              onClick={() => setI(item.num)}
               className={
-                item.num === number
+                item.num === i
                   ? `${bellefair.className} w-10 h-10 rounded-full bg-white text-dark md:w-[3.75rem] md:h-[3.75rem] md:text-[1.5rem] xl:w-20 xl:h-20 xl:text-nums-desktop`
                   : `${bellefair.className} w-10 h-10 rounded-full border-2 border-light border-opacity-30 hover:border-opacity-1 hover:border-white duration-200 ease-out md:w-[3.75rem] md:h-[3.75rem] md:text-[1.5rem] xl:w-20 xl:h-20 xl:text-nums-desktop`
               }
@@ -62,12 +64,12 @@ export default function page() {
           <h2
             className={`${bellefair.className} mt-3 mb-2 text-[2rem] uppercase md:text-[2.5rem]`}
           >
-            {technology[techIndex(number)].tech}
+            {technology[techIndex(i)].tech}
           </h2>
           <p
             className={`${barlow.className} mx-6 text-para-mobile text-light text-balance md:text-para-tablet md:max-w-[458px]`}
           >
-            {technology[techIndex(number)].desc}
+            {technology[techIndex(i)].desc}
           </p>
         </div>
       </section>
