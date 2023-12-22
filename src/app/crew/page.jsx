19a884f6bc8a6e4707ec7e30f9edb0cce321a15c
Joseph Wilde <jwilde19@gmail.com>
2023-12-21 19:13:40 -0500

    Change layout shift to xl screen size

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -30,13 +30,13 @@ export default function page() {
     <main
       className={`${barlowCondensed.className} w-screen h-screen p-crew-small bg-crew-mobile bg-cover bg-center overflow-hidden xs:p-crew-mobile sm:bg-crew-tablet md:h-screen md:p-crew-tablet lg:bg-crew-desktop xl:p-crew-desktop`}
     >
-      <h1 className='mb-2 uppercase sm:pb-0 text-center text-sub-mobile md:text-sub-tablet lg:text-sub-desktop lg:text-left'>
+      <h1 className='mb-2 uppercase sm:pb-0 text-center text-sub-mobile md:text-sub-tablet xl:text-sub-desktop xl:text-left'>
         <span className='opacity-25 mr-4'>02</span>Meet Your Crew
       </h1>
-      <div className='h-full flex flex-col md:items-center lg:flex-row'>
+      <div className='h-full flex flex-col md:items-center xl:flex-row'>
         {/* crew image */}
         <section className='w-full grid items-end grow md:h-crew-tablet border-b-[1px] border-[#383B4B] md:border-0 md:order-last overflow-hidden'>
-          <div className='w-[13.875rem] h-[273px] md:w-crew-tablet md:h-crew-img-tablet mx-auto lg:mx-0 flex items-end overflow-hidden relative'>
+          <div className='w-[13.875rem] h-[273px] mx-auto flex items-end overflow-hidden relative md:w-crew-tablet md:h-crew-img-tablet xl:mx-0'>
             <Image
               src={crew[crewIndex(member)].path}
               alt={crew[crewIndex(member)].name}
@@ -54,36 +54,36 @@ export default function page() {
           </div>
         </section>
         {/* crew info */}
-        <section className='flex flex-col items-center grow text-center md:pt-[3.75rem] md:max-w-crew-tablet lg:mb-auto lg:pt-36 lg:h-[486px] lg:max-w-[600px] lg:items-start'>
+        <section className='flex flex-col items-center grow text-center md:pt-[3.75rem] md:max-w-crew-tablet xl:mb-auto xl:pt-36 xl:h-[486px] xl:max-w-[600px] xl:items-start'>
           <h2
-            className={`${bellefair.className} text-1rem uppercase opacity-50 md:text-1.5rem lg:text-2rem`}
+            className={`${bellefair.className} text-1rem uppercase opacity-50 md:text-1.5rem xl:text-[2rem]`}
           >
             {crew[crewIndex(member)].title}
           </h2>
           <h2
-            className={`${bellefair.className} pb-2 xs:pb-3 text-[1.5rem] uppercase whitespace-nowrap md:text-[2.5rem] lg:pb-8 lg:text-[3.5rem]`}
+            className={`${bellefair.className} pb-2 xs:pb-3 text-[1.5rem] uppercase whitespace-nowrap md:text-[2.5rem] xl:pb-8 xl:text-[3.5rem]`}
           >
             {crew[crewIndex(member)].name}
           </h2>
           <p
             className={`${
               barlow.className
-            } h-40 text-para-mobile text-light text-balance md:h-20 md:text-para-tablet sm:max-w-[37rem] lg:text-para-desktop lg:text-start md:px-[${para(
+            } h-40 text-para-mobile text-light text-balance md:h-20 md:text-para-tablet sm:max-w-[37rem] xl:text-para-desktop xl:text-start md:px-[${para(
               member
             )}]`}
           >
             {crew[crewIndex(member)].desc}
           </p>
           {/* menu buttons */}
-          <div className='py-5 flex gap-6 -order-1 xs:py-8 md:py-10 md:order-last lg:p-0 lg:gap-6 lg:mt-auto lg:-bottom-6'>
+          <div className='py-5 flex gap-6 -order-1 xs:py-8 md:py-10 md:order-last xl:p-0 xl:gap-6 xl:mt-auto xl:-bottom-6'>
             {crew.map((crew, index) => (
               <button
                 key={index}
                 onClick={() => setMember(crew.name)}
                 className={
                   member === crew.name
-                    ? 'w-[10px] h-[10px] lg:w-4 lg:h-4 rounded-full bg-white'
-                    : 'w-[10px] h-[10px] lg:w-4 lg:h-4 rounded-full bg-white opacity-[0.175]'
+                    ? 'w-[10px] h-[10px] xl:w-4 xl:h-4 rounded-full bg-white'
+                    : 'w-[10px] h-[10px] xl:w-4 xl:h-4 rounded-full bg-white opacity-[0.175]'
                 }
               ></button>
             ))}
