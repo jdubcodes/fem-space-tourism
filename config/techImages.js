3c46834042a66bb982c82ed6431247c234588b46
Joseph Wilde <jwilde19@gmail.com>
2023-12-23 19:11:59 -0500

    Add Images function

--- config/techImages.js
+++ config/techImages.js
@@ -5,14 +5,23 @@ import capsuleDesktop from '/public/technology/image-space-capsule-portrait.jpg'
 import spaceportMobile from '/public/technology/image-spaceport-landscape.jpg'
 import spaceportDesktop from '/public/technology/image-spaceport-portrait.jpg'
 
-export const techImg = (i) => {
+export const techImg = (i, w) => {
   if (i == 1) {
+    if (w >= 1280) {
+      return launchDesktop
+    }
     return launchMobile
   }
   if (i == 2) {
+    if (w >= 1280) {
+      return capsuleDesktop
+    }
     return capsuleMobile
   }
   if (i == 3) {
+    if (w >= 1280) {
+      return spaceportDesktop
+    }
     return spaceportMobile
   }
 }
