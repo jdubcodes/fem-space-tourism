9b9e00b0e091615d9457df210b58c45fed02ca02
Joseph Wilde <jwilde19@gmail.com>
2024-01-11 15:31:40 -0500

    Change padding for technology desktop screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         'crew-desktop': '25vh 10.45rem 0rem',
         'tech-mobile': '6.25rem 0 0',
         'tech-tablet': '10.875rem 2.5rem 0',
-        'tech-desktop': '10.875rem 0 0',
+        'tech-desktop': '25vh 0 0',
       },
       width: {
         'nav-line': '29.875rem',
